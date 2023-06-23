13f584b477b417f321d9f4c747c99d5b0f32a5af
rafa-vasconcelos <rafaelvasconcelos13@gmail.com>
2023-06-23 14:22:07 -0300

    intro change

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,8 +2,8 @@ const Intro = () => {
   return (
     <section className="d-flex justify-content-center">
       <p>
-        Estou em busca de experiência profissional como desenvolvedor web full
-        stack. Acredito no aprendizado contínuo e estou sempre disposto para
+        Meu nome é Rafael, sou desenvolvedor web full stack, com foco em front
+        end. Acredito no aprendizado contínuo e estou sempre disposto para
         enfrentar novos desafios. Sou um grande estusiasta do desenvolvimento
         web e busco sempre atualizar meus conhecimentos.
       </p>
